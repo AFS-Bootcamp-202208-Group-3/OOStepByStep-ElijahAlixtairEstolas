9b95c6fdb47fefd031f3d35fdd590ab89f368784
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-08-31 17:19:36 +0800

    feat: added Klass implementation and Klass methods for student and refactor: introduce method to add ClassNumber

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,12 +1,22 @@
 package ooss;
 
 public class Student extends Person{
+    private Klass klass;
+
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
     @Override
     public String introduce() {
-        return super.introduce() + " I am a student.";
+        return super.introduce() + " I am a student."+" I am in class "+this.klass.getNumber();
+    }
+
+    public boolean isIn(int classNumber){
+        return classNumber== klass.getNumber();
+    }
+
+    public void join(int classNumber){
+        this.klass.setNumber(classNumber);
     }
 }
